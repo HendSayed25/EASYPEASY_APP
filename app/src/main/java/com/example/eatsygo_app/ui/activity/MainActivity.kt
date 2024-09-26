5433924b2117778f31f0502516f0a313fc8e18ed
Hend Sayed <hendsayed853@gmail.com>
2024-09-27 01:34:56 +0300

    Delete The Shadow Of BottomNavigation.. Change The images of onBoard Screens..

--- app/src/main/java/com/example/eatsygo_app/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/eatsygo_app/ui/activity/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.eatsygo_app.ui.activity
 
+import android.content.res.ColorStateList
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.eatsygo_app.R
@@ -11,27 +12,17 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.EntryPoint
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var bottomNavigation: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val navController = findNavController(R.id.container_main)
-        bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNavigation.setupWithNavController(navController)
 
     }
-
-    fun hideBottomNavigation(){
-        bottomNavigation.visibility = View.GONE
-    }
-
-    fun showBottomNavigation(){
-        bottomNavigation.visibility = View.VISIBLE
-    }
-
 }
